dc62cd26f1f18650ff2a83b04ebb75fa43dc1831
Vincent Spitale <43227465+vincentspitale@users.noreply.github.com>
2020-08-30 14:21:10 -0400

    Personalized

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,15 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to my site.</p>
+    <SEO title="Welcome" />
+    <p>Coming soon.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
     </div>
-    <Link to="/page-2/">About</Link> <br />
+    <Link to="/about/">About</Link> <br />
   </Layout>
 )
 
