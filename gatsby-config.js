589482f51a6537732a5fd66f11dad6e2b1e7226c
Vincent Spitale <43227465+vincentspitale@users.noreply.github.com>
2020-08-30 13:38:29 -0400

    Personalized

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Vincent Spitale`,
+    description: `My personal site.`,
+    author: `Vincent Spitale`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
