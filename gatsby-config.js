6cd070995741b221964d4e42217e564942fadd5c
Vincent Spitale <43227465+vincentspitale@users.noreply.github.com>
2020-08-31 19:29:36 -0400

    Removed gatsby file system assets

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,13 +13,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `assets`,
-        path: `${__dirname}/src/assets/`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
