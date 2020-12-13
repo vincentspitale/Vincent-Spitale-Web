535aa9793b853341c824ceb3f49e2b7ed093f140
Vincent Spitale <43227465+vincentspitale@users.noreply.github.com>
2020-12-12 22:12:14 -0500

    Added Memoji Glyph

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/VS.svg`,  //This path is relative to the root of the site.
+        icon: `src/assets/memoji.png`,  //This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
