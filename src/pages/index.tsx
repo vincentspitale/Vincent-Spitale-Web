e723fe275ca7873b7a0eecffc8768ac90f7bb426
Vincent Spitale <43227465+vincentspitale@users.noreply.github.com>
2021-01-01 12:05:09 -0500

    Smaller profile on mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,9 @@ const IndexPage = () => (
     <div className="Padding">
       <div className="HeroGroup">
       <div className="HeroTextGroup">
+        <div className="SmallProfile">
+      <img src={require('../images/me2.jpg')}/>
+        </div>
         <p>Hey there, I'm</p>
         <h1>Vincent <span className="lastName">Spitale</span></h1>
         <p>a student, software engineer, and diversity advocate</p>
