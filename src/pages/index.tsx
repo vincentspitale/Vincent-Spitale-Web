0338234515cef0f10da163ac41afe4397bf39142
Vincent Spitale <43227465+vincentspitale@users.noreply.github.com>
2021-01-01 02:35:46 -0500

    Flipped Sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,20 +50,8 @@ const IndexPage = () => (
 
     <div className="Padding">
     <div className="InfoSections">
-      <Section>
-      <div className="Important">
-      <p>I'm a <a href="https://pronoun.is/he?or=they" target="_blank">
-      he/they</a> studying computer science and mathematics at Northeastern University. 
-      <br/>
-      <br/>
-      Outside of class, I experiment with app development and augmented reality tech. I've found AR to be a perfect way for me to bring togther my love for visual and technical detail. I also lead workshops that aim to make the tech industry accessible and inviting to everyone. I'm currently in charge of design at <a href="https://numulti.com" target="_blank">MULTI</a>, Northeastern's undergraduate students for diversity & inclusion in tech.
-      <br/>
-      <br/>
-      If you think I might be the right fit for a role, please reach out.
-      </p>
-      </div>
-      </Section>
-      <Section>
+
+    <Section>
       <ClientOnly>
         <Spotify />
         </ClientOnly>
@@ -110,6 +98,19 @@ const IndexPage = () => (
         <p>Functional Programming</p>
         </div>
         </div>
+      </Section>
+      <Section>
+      <div className="Important">
+      <p>I'm a <a href="https://pronoun.is/he?or=they" target="_blank">
+      he/they</a> studying computer science and mathematics at Northeastern University. 
+      <br/>
+      <br/>
+      Outside of class, I experiment with app development and augmented reality tech. I've found AR to be a perfect way for me to bring togther my love for visual and technical detail. I also lead workshops that aim to make the tech industry accessible and inviting to everyone. I'm currently in charge of design at <a href="https://numulti.com" target="_blank">MULTI</a>, Northeastern's undergraduate students for diversity & inclusion in tech.
+      <br/>
+      <br/>
+      If you think I might be the right fit for a role, please reach out.
+      </p>
+      </div>
       </Section>
       
     </div>
