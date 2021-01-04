08bcf46050b0bb0550ecf0b984016616e5433d69
Vincent Spitale <43227465+vincentspitale@users.noreply.github.com>
2021-01-03 23:03:18 -0500

    Changed names

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,12 +28,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Vincent Spitale`,
+        short_name: `Vincent`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
         icon: `src/assets/memoji.png`,  //This path is relative to the root of the site.
       },
     },
